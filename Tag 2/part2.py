d04e0c1bef3191bff99d2914e9b20cc38e36f72b
Huwayi <75526823+huwayi95@users.noreply.github.com>
2021-12-02 11:03:19 +0100

    Update part2.py

--- Tag 2/part2.py
+++ Tag 2/part2.py
@@ -1,27 +1,16 @@
-input = open("input.txt", "rt")
-input2 = [i.strip() for i in input.readlines()]
-#input2 =[int(i) for i in input.readlines()]
-test_data = ["forward 5","down 5","forward 8","up 3","down 8","forward 2"]
+with open("input.txt", "rt") as text:
+    input2 = [i.strip() for i in text.readlines()]
 
-down=0
-aim=0
+test_data = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
+down, aim, forward = 0, 0, 0
 
-forward2 = 0
-depth = 0
 for data in input2:
     if data.startswith("forward"):
-        forward2+=int(data.split(" ")[1])
-        down+=(aim*int(data.split(" ")[1]))
+        forward += int(data.split(" ")[1])
+        down += (aim*int(data.split(" ")[1]))
     if data.startswith("up"):
         aim -= int(data.split(" ")[1])
     if data.startswith("down"):
         aim += int(data.split(" ")[1])
 
-print("Horizontal position: ", forward2,"\nDepth: ", down,"\nSolution: ", forward2*down)
-
-
-
-
-
-
-
+print("Horizontal position: ", forward, "\nDepth: ", down, "\nSolution: ", forward*down)
